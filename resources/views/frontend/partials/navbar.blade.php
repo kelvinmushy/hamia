85a59be4b7fd416f40e0d14d8c63614210cbd76b
kelvinmushy <74298025+kelvinmushy@users.noreply.github.com>
2025-03-29 01:38:22 +0300

    fix:data

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -1,17 +1,18 @@
+<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-primary">
+  <a class="navbar-brand" href="/">
+    <img src="{{ url('/images/logo/logo.png') }}" style="height:50px;width:250px;margin:12px" />
+  </a>
 
-<nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">
-      <img src="{{ url('/images/logo/logo.png') }}" alt="Logo" style="height:60px;width:auto;margin:12px" />
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-      ☰
-    </button>
-    <div class="collapse navbar-collapse" id="navbar-collapse">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 m-auto navMenu">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse">☰</button>
+
+  <div class="collapse navbar-collapse" id="navbar-collapse">
+    <div class="d-flex w-100 justify-content-between">
+      <!-- Center-aligned items (Agents, Client Requests) -->
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
         <li class="nav-item" style="font-size: large">
           <a class="nav-link" href="{{ route('agents') }}">Agents</a>
         </li>
+
         @guest
           <li class="nav-item" style="font-size: large">
             <a class="nav-link" href="#" id="btnClient">Client Requests</a>
@@ -20,45 +21,47 @@
           <li class="nav-item" style="font-size: large">
             <a class="nav-link" href="/client/request">Client Requests</a>
           </li>
-        @endguest
+        @endif
       </ul>
+
+      <!-- Right-aligned items (Sign In, Sign Up, SELL) -->
       <ul class="navbar-nav">
         @guest
           <li class="nav-item" style="font-size: large">
-            <a class="nav-link" href="#" id="btnLogin" >Sign In</a> |
+            <a class="nav-link" href="#" id="btnLogin">Sign In</a> |
             <a class="nav-link" href="#" id="btnRegister">Registering</a>
           </li>
-          <li class="nav-item" style="font-size: large">
-            <a class="nav-link" href="#" id="btnSell" style="color:orange;">+ SELL</a>
+          <li class="nav-item">
+            <a href="#" id="btnSell" class="nav-link" style="color:orange; font-size: large;margin-right:2px">SELL</a>
           </li>
         @else
-          <li class="nav-item dropdown" style="margin-top:9px">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <li class="nav-item dropdown" style="margin-top:9px;">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: large;">
               {{ ucfirst(Auth::user()->username) }}
+              <i class="material-icons right">arrow_drop_down</i>
             </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               @if(Auth::user()->role->id == 1)
-                <li>
-                  <a class="dropdown-item" href="{{ route('agent.dashboard') }}">
-                    <i class="fa fa-user"></i> Dashboard
-                  </a>
-                </li>
-              @elseif(Auth::user()->role->id == 2)
-                <li>
-                  <a class="dropdown-item" href="{{ route('agent.dashboard') }}">
-                    <i class="fa fa-user"></i> Dashboard
-                  </a>
-                </li>
-              @endif
-              <li>
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                  <i class="fa fa-power-off"></i> Logout
+                <a href="{{ route('agent.dashboard') }}" class="dropdown-item" style="font-size: large;">
+                  <i class="fa fa-user"></i> Dashboard
                 </a>
-              </li>
-            </ul>
+              @elseif(Auth::user()->role->id == 2)
+                <a href="{{ route('agent.dashboard') }}" class="dropdown-item" style="font-size: large;">
+                  <i class="fa fa-user"></i> Dashboard
+                </a>
+              @endif
+              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" style="font-size: large;">
+                <i class="fa fa-power-off"></i> Logout
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </div>
           </li>
-          <li class="nav-item" >
-            <a class="nav-link" href="/agent/properties/create" style="color:orange;padding:2px">SELL</a>
+
+          <li class="nav-item">
+            <a href="/agent/properties/create" class="nav-link" style="color:orange; font-size: large;">SELL</a>
           </li>
         @endguest
       </ul>
